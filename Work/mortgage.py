f90f46e3f0b924a61353bf9aa875d22da6fc29cb
Bob McDonald <UltraBob@users.noreply.github.com>
2021-05-31 09:37:48 +0900

    mortgage example with all extra exercises

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,28 @@
 # mortgage.py
 #
 # Exercise 1.7
+extra_payment_start_month = 5 * 12 + 1
+extra_payment_end_month = 10 * 12 - 1
+extra_payment = 1000
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+
+while principal > 0:
+    month += 1
+    if extra_payment_start_month <= month <= extra_payment_end_month:
+        additional = extra_payment
+    else:
+        additional = 0
+    principal = principal * (1 + rate / 12) - payment - additional
+    if principal < 0:
+        overpay = abs(0 - principal)
+        additional -= overpay
+        principal = 0
+    total_paid = total_paid + payment + additional
+    print(month, total_paid, principal)
+
+print(f"Paid {total_paid} in {month} months.")
